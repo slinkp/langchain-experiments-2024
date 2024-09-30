83df23157367d14b273f9d4f8dcc276f0435fc46
Paul Winkler <slinkp@gmail.com>
2024-09-30 17:48:19 -0400

    Refactor into main()

--- stateless-bot.py
+++ stateless-bot.py
@@ -1,29 +1,38 @@
 import os
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain_pinecone import PineconeVectorStore
+import warnings
 
+warnings.filterwarnings("ignore")
 
-print("Step 1. Loading knowledge")
-embeddings = OpenAIEmbeddings()
-vectorstore = PineconeVectorStore(
+chat_history = []
+
+def main():
+    print("Step 1. Loading knowledge")
+    embeddings = OpenAIEmbeddings()
+    vectorstore = PineconeVectorStore(
         index_name=os.environ["PINECONE_INDEX_NAME"], embedding=embeddings
-)
+    )
 
-print("Step 2, building RAG chain and asking questions")
-chat = ChatOpenAI(verbose=True, temperature=0, model_name="gpt-3.5-turbo")
+    print("Step 2, building RAG chain and asking questions")
+    chat = ChatOpenAI(verbose=True, temperature=0, model_name="gpt-3.5-turbo")
 
-qa = RetrievalQA.from_chain_type(
-    llm=chat, chain_type="stuff", retriever=vectorstore.as_retriever()
-)
+    qa = RetrievalQA.from_chain_type(
+        llm=chat, chain_type="stuff", retriever=vectorstore.as_retriever()
+    )
 
-prompts = (
-    "What are the applications of generative AI according to the paper? Please number each application.",
-    "Can you please elaborate more on application number 2?"
-)
+    prompts = (
+        "What are the applications of generative AI according to the paper? Please number each application.",
+        "Can you please elaborate more on application number 2?"
+        )
 
-for prompt in prompts:
-    res = qa.invoke(prompt)
-    print(f"Q: {res['query']}")
-    print('-----------------------------------')
-    print(f"A: {res['result']}\n\n")
+    for prompt in prompts:
+        res = qa.invoke(prompt)
+        print(f"Q: {res['query']}")
+        print('-----------------------------------')
+        print(f"A: {res['result']}\n\n")
+
+if __name__ == "__main__":
+    main()
