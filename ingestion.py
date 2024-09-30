e785b4fe32d2297bc079f69a27913303535abcd7
Paul Winkler <slinkp@gmail.com>
2024-09-30 16:44:56 -0400

    Step 3. Split text into chunks

--- ingestion.py
+++ ingestion.py
@@ -5,7 +5,18 @@ from langchain_anthropic import ChatAnthropic
 from langchain_pinecone import PineconeVectorStore
 
 
-loader = PyPDFLoader("data/impact_of_generativeAI.pdf")
+path = os.path.join(os.path.dirname(__file__), "data/impact_of_generativeAI.pdf")
+loader = PyPDFLoader(path)
 document = loader.load()
+print(f"loaded {path}")
+
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+texts = text_splitter.split_documents(document)
+print(f"created {len(texts)} chunks")
+
+# for i, text in enumerate(texts):
+#     print(f"chunk {i}\n==========================================\n")
+#     print(text)
+
 
 # chat = ChatAnthropic(model_name="claude-3-5-sonnet-20240620")
